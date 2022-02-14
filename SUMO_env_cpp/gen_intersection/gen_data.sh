b7145e7015f034bdde8c1e4b8a4048399c1eb379
王奕智 <franksum3109@gmail.com>
2022-02-15 00:31:52 +0800

    Update code for Linux

--- SUMO_env_cpp/gen_intersection/gen_data.sh
+++ SUMO_env_cpp/gen_intersection/gen_data.sh
@@ -7,7 +7,7 @@ cd gen_intersection
 
 echo $1
 ./gen_inter $1 inter_data/lane$1.txt
-python gen_inter_info_v3.py inter_data/lane$1.txt lane_info$1
+python3 gen_inter_info_v3.py inter_data/lane$1.txt lane_info$1
 
 ./gen_advise $1 advise_data/advise$1.txt
-python gen_advise_info.py advise_data/advise$1.txt advise_info$1
+python3 gen_advise_info.py advise_data/advise$1.txt advise_info$1
